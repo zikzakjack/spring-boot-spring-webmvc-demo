c5342cd78678516980771b7ff55232f4304d88f3
zikzakjack <kalyansmiles@gmail.com>
2020-04-23 22:56:54 +0530

    Created Initial skeleton project with spring boot and spring webmvc

---
+++ src/main/java/zikzakjack/SpringBootSpringWebmvcApplication.java
@@ -0,0 +1,13 @@
+package zikzakjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringWebmvcApplication.class, args);
+	}
+
+}
